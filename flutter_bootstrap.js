7f8a3c71a791075adda12163fcd3a6a1e03164e0
Koidio <koidioble@gmail.com>
2024-08-07 00:26:39 +0000

    yannble

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"b73367a30e9b9f5056e41223dbae85c2578949a5","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"b73367a30e9b9f5056e41223dbae85c2578949a5","builds":[{"compileTarget":"dart2js","renderer":"html","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2301219455"
+    serviceWorkerVersion: "2329740533"
   }
 });
