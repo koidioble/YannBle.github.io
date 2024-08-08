1c2bc9058a45ef0db2365eacd2b92b9663b8a3d0
Koidio <koidioble@gmail.com>
2024-08-08 17:46:45 +0000

    Update main.dart and index.html

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Yann Blé',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lime),
         //   fontFamily: GoogleFonts.tinos().fontFamily,
