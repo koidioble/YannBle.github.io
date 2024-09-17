7f466b1dc395de55ec306c68dfe878094f560ab2
Koidio <koidioble@gmail.com>
2024-09-16 21:51:19 -0400

    modified updates

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter_bootstrap.js": "9ddf1a7769b1d7108c5601ebed801d4d",
 "version.json": "9e1efd4735fb681eef211e45bb73d9d6",
-"index.html": "37cc7fbef48516ef8a06fcf36c79a8bf",
-"/": "37cc7fbef48516ef8a06fcf36c79a8bf",
+"index.html": "7cc6656f3561a4a623586a3280b428f7",
+"/": "7cc6656f3561a4a623586a3280b428f7",
 "main.dart.js": "84b638b2257020ff951a067e6400dfb2",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "6b650c4cdd49dfc011ad87216a2f19f3",
