a60dc17f6925ea3fae573c8b22ceea00dda0d80c
Koidio <koidioble@gmail.com>
2024-10-17 13:37:13 -0400

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:yannble/pages/home/home_page1.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Koidio (Y.) Blé - Portfolio',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
+        fontFamily: GoogleFonts.ubuntu().fontFamily,
+        useMaterial3: true,
+      ),
+      home: const HomePage1(),
+    );
+  }
+}
